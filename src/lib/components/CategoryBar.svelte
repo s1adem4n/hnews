ac8e7448aa4963386701307d6998f909375c8925
Jonathan <jonathanbuko@icloud.com>
2023-09-24 21:05:29 +0200

    add category bar hover effect, fix comment pre overflow

--- src/lib/components/CategoryBar.svelte
+++ src/lib/components/CategoryBar.svelte
@@ -17,7 +17,7 @@
 	{#each CATEGORIES as CATEGORY, i}
 		<a
 			href="/{CATEGORY}{paginated ? '/1' : ''}"
-			class="flex-1 text-center py-2"
+			class="flex-1 text-center py-2 hover:bg-surface0 transition-colors rounded-md"
 			class:font-bold={CATEGORY === category}
 		>
 			{CATEGORY}
