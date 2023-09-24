a135eb9673e66156d5f4d7f2fe75eaa5bc4ffc5b
Jonathan <jonathanbuko@icloud.com>
2023-09-24 18:48:28 +0200

    add title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,9 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Hacker News</title>
+	<meta name="description" content="Hacker News clone built with SvelteKit" />
+</svelte:head>
+
 <slot />
